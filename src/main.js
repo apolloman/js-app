f6d5c7bc90fc9091176de2b2b90a0e81e1acd8ca
Roma Mus <apollo5577@gmail.com>
2018-03-23 18:25:14 +0700

    some changes

--- src/main.js
+++ src/main.js
@@ -1,45 +1,53 @@
 class ToDoClass {
 	constructor() {
 		debugger;
-    /* Locale Storage */
-    this.tasks = JSON.parse(localStorage.getItem("TASKS"));
+		/* Locale Storage */
+		this.tasks = JSON.parse(localStorage.getItem("TASKS"));
 
-/* List of tasks */
-   if(!this.tasks) {
-      this.tasks = [
-          {task: 'Learn ReactJS', isComplete: false},
-          {task: 'Clean My Home', isComplete: true},
-          {task: 'Buy something to eat', isComplete: false},
-	   ];
-	}
+		/* List of tasks */
+		if (!this.tasks) {
+			this.tasks = [{
+					task: 'Learn ReactJS',
+					isComplete: false
+				},
+				{
+					task: 'Clean My Home',
+					isComplete: true
+				},
+				{
+					task: 'Buy something to eat',
+					isComplete: false
+				},
+			];
+		}
 
-    this.addKeytListener();
-	this.loadTasks();
+		this.addKeytListener();
+		this.loadTasks();
 
 	} /* end constructor */
 
 
-/* add Item with Enter Button */
-	addKeyListener() { 
-    	document.getElementById('addTask').addEventListener('keypress', event => {
-            if(event.keyCode === 13) {
-            	this.addTask(event.target.value);
-            	event.target.value = '';
-            }
-    	});
-    }
+	/* add Item with Enter Button */
+	addKeyListener() {
+		document.getElementById('addTask').addEventListener('keypress', event => {
+			if (event.keyCode === 13) {
+				this.addTask(event.target.value);
+				event.target.value = '';
+			}
+		});
+	}
 
- 
-/* Iterate over tasks in array and add to list */
+
+	/* Iterate over tasks in array and add to list */
 	loadTasks() {
-		let tasksHtml = this.tasks.reduce((html,task,index) => html += this.generateTaskHtml(task, index), '');
+		let tasksHtml = this.tasks.reduce((html, task, index) => html += this.generateTaskHtml(task, index), '');
 		document.getElementById('taskList').innerHTML = tasksHtml;
 		localStorage.setItem("TASKS", JSON.stringify(this.tasks));
 	}
 
 
-    generateTaskHtml(task, index) {
-        return `
+	generateTaskHtml(task, index) {
+		return `
 <li class="list-group-item checkbox">
 	<div class="row">
 		<div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 checkbox">
@@ -57,40 +65,40 @@ class ToDoClass {
 		</div>
 	</div>
 </li>`;
-}
+	}
 
-    toggleTaskStatus(index) {
-	    this.tasks[index].isComplete = !this.tasks[index].isComplete;
-	    this.loadTasks();
-    }
+	toggleTaskStatus(index) {
+		this.tasks[index].isComplete = !this.tasks[index].isComplete;
+		this.loadTasks();
+	}
 
-    deleteTask(event, taskIndex) {
-        event.preventDefault();
-        this.tasks.splice(taskIndex, 1);
-        this.loadTasks();
-    }
+	deleteTask(event, taskIndex) {
+		event.preventDefault();
+		this.tasks.splice(taskIndex, 1);
+		this.loadTasks();
+	}
 
-    addTaskClick() {
-  	   let target = document.getElementById('addTask');
-  	   this.addTask(target.value);
-  	   target.value = "";
-    }
+	addTaskClick() {
+		let target = document.getElementById('addTask');
+		this.addTask(target.value);
+		target.value = "";
+	}
 
-    addTask(task) {
-       let newTask = {
-         task,
-         isComplete: false,
-     };
+	addTask(task) {
+		let newTask = {
+			task,
+			isComplete: false,
+		};
 
-     let parentDiv = document.getElementById('addTask');
-     if (task === '') {
-     	parentDiv.classList.add('is-invalid');
-     } else {
-     	parentDiv.classList.remove('is-invalid');
-     	this.tasks.push(newTask);
-     	this.loadTasks();
-     }
-  }
+		let parentDiv = document.getElementById('addTask');
+		if (task === '') {
+			parentDiv.classList.add('is-invalid');
+		} else {
+			parentDiv.classList.remove('is-invalid');
+			this.tasks.push(newTask);
+			this.loadTasks();
+		}
+	}
 
 } /* End */
 
@@ -98,4 +106,3 @@ let toDo;
 window.addEventListener("load", () => {
 	toDo = new ToDoClass();
 });
-
